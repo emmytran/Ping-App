daec9353f2be32dd49c1109851de6f633e397889
lor93 <Elvislor01@gmail.com>
2022-09-27 16:29:46 -0700

    Created the Contact and maybe the Home page for the app.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,12 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {path: '', redirectTo: 'home' , pathMatch: 'full'},
-  {path: 'home', loadChildren: () => import('./home/home.module').then(m => m.homePageModule)}
+  {path: 'home', loadChildren: () => import('./home/home.module').then(m => m.homePageModule)},
   {
     path: 'Contacts/:id',
     loadChildren: () => import('./Contacts/Contacts.module').then(m => ContactPageModule)
-  },  {
+  },
+  {
     path: 'contacts',
     loadChildren: () => import('./contacts/contacts.module').then( m => m.ContactsPageModule)
   }
