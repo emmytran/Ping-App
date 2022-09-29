aeeef1f203b457a38b626b62e4956b54b64fb0bb
lor93 <Elvislor01@gmail.com>
2022-09-29 16:28:20 -0700

    Dont worry about added to run it on android

--- ionic-form-validation/src/app/home/home.page.ts
+++ ionic-form-validation/src/app/home/home.page.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators} from "@angular/forms";
-import { DbService } from './../services/db.services'
+import { DbService } from '../service/db.service';
 import { ToastController } from '@ionic/angular';
 import { Router } from "@angular/router";
 
