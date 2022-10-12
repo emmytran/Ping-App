76d107054531ab2590e736c191da3897940c1496
lor93 <Elvislor01@gmail.com>
2022-10-11 21:42:39 -0700

    Made some change to the header to make it connect better.

--- src/app/services/db.service.ts
+++ src/app/services/db.service.ts
@@ -1,8 +1,8 @@
 import { Injectable } from '@angular/core';
 import { Platform } from '@ionic/angular';
-//import { Contacts } from './contacts';
+import { Contacts } from './contacts';
 import { HttpClient } from '@angular/common/http';
-//import { BehaviorSubject, Observable } from 'rxjs';
+import { BehaviorSubject, Observable } from 'rxjs';
 import { SQLitePorter } from '@ionic-native/sqlite-porter/ngx';
 import { SQLite, SQLiteObject } from '@ionic-native/sqlite/ngx';
 import { PromiseType } from 'protractor/built/plugins';
