099ed226a65caaf9721405f0b34fccffb4cf83f5
lor93 <Elvislor01@gmail.com>
2022-10-14 09:05:31 -0700

    Ran into some issues last night due to the fact that alot of the syntax were out of date so working on updating all of the syntax issues.

---
+++ src/app/services/database.service.ts
@@ -0,0 +1,70 @@
+import { Injectable, INJECTOR } from "@angular/core";
+import { Plugins } from '@capacitor/core';
+import '@capacitor-community/sqlite';
+import { AlertController } from '@ionic/angular';
+import { HttpClient } from "@angular/common/http";
+import { BehaviorSubject, from, of } from "rxjs";
+import { switchMap } from 'rxjs/operators';
+
+import { JsonSQLite } from "@capacitor-community/sqlite";
+//import { info } from "console";
+const  { CapacitorSQLite, Device, Storage } = Plugins;
+
+const DB_SETUP_KEY = 'first_db_setup';
+const DB_NAME_KEY =  'db_name';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DatabaseService {
+  dbReady = new BehaviorSubject(false);
+  dbName = '';
+  
+  constructor(private http: HttpClient, private alertCrtl: AlertController) { }
+  
+  async  init() : Promise<void> {
+   const infor= await Device.getinfo();
+   
+   if (infor.platform === "android") {
+    try {
+      const sqlite = CapacitorSQLite as any;
+      await sqlite.requestPermissons();
+      this.setupDatabase();
+    }catch (e) {
+      const alert = await this.alertCrtl.create({
+        header: 'No DB access',
+        subHeader: "This app can't work without Database access.",
+        buttons: ['Ok']
+      });
+      await alert.present();
+    }
+   }else {
+    this.setupDatabase();
+   }
+  }
+
+  private async setupDatabase() {
+    const dbSetupDone = await Storage.get({key: DB_SETUP_KEY});
+
+    if(!dbSetupDone.value) {
+      this.downloadDatabase();
+    }else{
+      this.dbName = (await Storage.get({key: DB_NAME_KEY})).value;
+      await CapacitorSQLite.open({database: this.dbName});
+      this.dbReady.next(true);
+    }
+  }
+  private downloadDatabase(update = false) {
+    this.http.get('').subscribe(async (jsonExport: JsonSQLite) => {
+      const jsonstring = JSON.stringify(jsonExport);
+      const isvalid = await CapacitorSQLite.isJsonValid({jsonstring});
+
+      if (!update) {
+        await CapacitorSQLite.createSyncTable();
+      }else {
+        await CapacitorSQLite.setSyncDate({syncdate: '' + new Date().getTime ()})
+      }
+      this.dbReady.next(true);
+    });
+  }
+}
