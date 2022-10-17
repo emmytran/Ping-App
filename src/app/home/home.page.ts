a2b18c48ce6879b4bbcee519bd4b2cabf3484bf1
lor93 <Elvislor01@gmail.com>
2022-10-17 10:39:36 -0700

    Made some update to the push notifcation setting

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,14 +1,55 @@
-import { Component } from '@angular/core';
+import { Component, NgProbeToken, OnInit } from '@angular/core';
 import { DatabaseService } from '../services/database.service';
+import{
+  ActionPerformed,
+  PushNotificationSchema,
+  PushNotifications,
+  Token,
+} from '@capacitor/push-notifications';
+
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+
+export class HomePage implements OnInit{
   contacts = [];
   export = null;
   newContacts = 'New Contacts';
+  ngOnInit() {
+    console.log('Initializing HomePage');
+
+    //Request permission to use push notifications
+    //Ios will prompt user and return if they are granted permisson or not
+    //Android will just grant permisson without prompting
+    PushNotifications.requestPermissions().then(result => {
+      if(result.receive === 'granted') {
+        //Register with Apple / Google to recieve push via APNS/FCM
+        PushNotifications.register();
+      }else {
+        //Show some error
+      }
+    });
+    PushNotifications.addListener('registration', (token: Token) => {
+      alert('Push registration success, token: ' + token.value);
+    });
+    PushNotifications.addListener('registrationError', (error: any) => {
+      alert('Error on registration: ' + JSON.stringify(error));
+    });
+    PushNotifications.addListener(
+      'pushNotificationReceived',
+      (notification: PushNotificationSchema) => {
+        alert('Push  receive: ' + JSON.stringify(notification));
+      },
+    );
+    PushNotifications.addListener(
+      'pushNotificationActionPerformed',
+      (notification: ActionPerformed) => {
+        alert('Push action performed: ' + JSON.stringify(notification));
+      },
+    );
+  }
 
   constructor(private databaseService: DatabaseService) {
     this.loadContacts();
